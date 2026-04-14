88db8825cc49ed751e63f3531d37093642015a70
spanwite <spanwite@ya.ru>
2026-04-14 10:40:01 +0300

    feat: Implement search query clear button

--- src/components/Todo/TodoSearchForm.tsx
+++ src/components/Todo/TodoSearchForm.tsx
@@ -1,9 +1,10 @@
 import { useTodoContext } from '@/hooks/useTodoContext';
+import { Search, X } from 'lucide-preact';
+import { useId } from 'preact/hooks';
+import { Field } from '../ui/Field';
 import { Input } from '../ui/Input';
 import Label from '../ui/Label';
-import { Field } from '../ui/Field';
-import { Search } from 'lucide-preact';
-import { useId } from 'preact/hooks';
+import { cn } from '@/lib/helpers';
 
 export function TodoSearchForm() {
   const {
@@ -19,13 +20,24 @@ export function TodoSearchForm() {
           <Search className='text-muted-foreground pointer-events-none absolute top-1/2 left-2.5 h-4 w-4 -translate-y-1/2' />
           <Input
             id={searchQueryInputId}
-            className='pl-8'
+            className='peer px-8'
             autoComplete='off'
             type='text'
             value={searchQuery.value}
             onInput={(e) => (searchQuery.value = e.currentTarget.value)}
             placeholder='Покормить собаку...'
           />
+          <button
+            type='button'
+            onClick={() => (searchQuery.value = '')}
+            className={cn(
+              'pointer-events-none absolute top-1/2 right-2.5 -translate-y-1/2 cursor-pointer rounded-sm p-0.5 text-transparent transition-colors',
+              'hover:bg-muted-background',
+              'peer-not-placeholder-shown:text-muted-foreground peer-not-placeholder-shown:pointer-events-auto'
+            )}
+          >
+            <X />
+          </button>
         </div>
       </Field>
     </form>
