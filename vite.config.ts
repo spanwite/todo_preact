e9900472a7fa25711d397692df6b6b0ae1fa3cf4
spanwite <spanwite@ya.ru>
2026-04-15 22:47:14 +0300

    ci: fix html paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import tailwindcss from '@tailwindcss/vite';
+import { loadEnv } from 'vite';
+
+const { VITE_BASE_URL } = loadEnv(process.env.NODE_ENV!, process.cwd(), '');
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +13,5 @@ export default defineConfig({
       '@': '/src',
     },
   },
+  base: VITE_BASE_URL ?? '/',
 });
